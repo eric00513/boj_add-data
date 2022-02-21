7c66b7939b96f198c3b9d66d0a800df383ea087f
eric00513 <37246466+eric00513@users.noreply.github.com>
2022-02-22 03:43:40 +0900

    Update boj9251_tle-gen3.cpp

--- boj9251/tle/gen/boj9251_tle-gen3.cpp
+++ boj9251/tle/gen/boj9251_tle-gen3.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
-    string s(500 * 2);
+    string s(500 * 2, '.');
     for (int i = 0; i < 500 * 2; i += 2) s[i] = 'H', s[i + 1] = 'I'; 
     cout << s << '\n';
     reverse(s.begin(), s.end());
