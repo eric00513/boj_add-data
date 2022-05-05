7a7efabda2a814bc4a13fcd16366ac3e3b76ea99
eric00513 <37246466+eric00513@users.noreply.github.com>
2022-05-05 14:29:30 +0900

    Update boj2042_tle-gen.cpp

--- boj2042/tle/gen/boj2042_tle-gen.cpp
+++ boj2042/tle/gen/boj2042_tle-gen.cpp
@@ -2,22 +2,22 @@
 #include "testlib.h"
 using namespace std;
 
-using ull = unsigned long long;
+using ll = long long;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 	registerGen(argc, argv, 1);
-	freopen("boj2042_tle03.in", "w", stdout);
+	freopen("boj2042_tle04.in", "w", stdout);
 	int n = rnd.next(990000, 1000000);
 	int m = rnd.next(9800, 10000);
 	int k = rnd.next(9800, 10000);
 	printf("%d %d %d\n", n, m, k);
 	for (int i = 0; i < n; i++) {
-		printf("%lld\n", rnd.next(-(LLONG_MAX / 2), LLONG_MAX / 2));
+		printf("%lld\n", rnd.next((ll)-9e13, (ll)9e13));
 	}
 	for (int i = 0; i < m; i++) {
-		printf("1 %d %lld\n", rnd.next(1, n), rnd.next(-(LLONG_MAX / 2), LLONG_MAX / 2));
+		printf("1 %d %lld\n", rnd.next(1, n), rnd.next((ll)-9e13, (ll)9e13));
 	}
 	for (int i = 0; i < k; i++) {
-		printf("2 %d %d\n", rnd.next(1, n / 5), rnd.next(n - 1000, n));
+		printf("2 %d %d\n", rnd.next(1, n / 4), rnd.next(n / 5 * 3, n));
 	}
 }
